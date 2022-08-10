296df1bdb5422b3e74c199f9e8bd75f53e1a0309
Влад <v4nkov.v@yandex.ru>
2022-08-10 22:35:45 +0300

    Страницу index.html разбил на компоненты (View, Presenter)

--- src/presenter/component-rendering-presenter.js
+++ src/presenter/component-rendering-presenter.js
@@ -7,17 +7,10 @@ export default class ComponentRenderingPresenter {
   init = (container) => {
     this.container = container;
 
-    //render(this.container, this.mainElement);
-    //render(new SortView(), this.boardComponent.getElement());
-    //render(this.taskListComponent, this.boardComponent.getElement());
-    //render(new TaskEditView(), this.taskListComponent.getElement());
-
     for (let i = 0; i < 5; i++) {
       render(new MovieCardView(), this.container);
     }
 
     render(new ButtonView(), this.container, RenderPosition.AFTEREND);
-
-    //render(new LoadMoreButtonView(), this.boardComponent.getElement());
   };
 }
